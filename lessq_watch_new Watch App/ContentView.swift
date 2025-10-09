7b6335d438a8254e0c2730ecc7e76a9b2ac61045
Yeski <yeskialex@gmail.com>
2025-10-09 16:53:21 +0900

    Initial Commit

---
+++ lessq_watch_new Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lessq_watch_new Watch App
+//
+//  Created by Yehezkiel Alexander on 10/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lessq_watch_new Watch App/lessq_watch_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  lessq_watch_newApp.swift
+//  lessq_watch_new Watch App
+//
+//  Created by Yehezkiel Alexander on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct lessq_watch_new_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
